d46f7f0ea742eb45f80c2fa2bf9191f318d9920b
talum <tracy@flatironschool.com>
2017-03-08 11:43:35 -0500

    initial configs for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../stylesheets/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The Royal Bank of Flatiron</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Here are your most recent transactions.
         </p>
       </div>
     );
